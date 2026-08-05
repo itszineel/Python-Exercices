e3b265159105bf430120061ef68cf75505c30022
unknown <itszineel@gmail.com>
2026-08-05 21:17:08 +0100

    exercise-python

--- DistanceBetweenTwoPoints.py
+++ DistanceBetweenTwoPoints.py
@@ -5,3 +5,4 @@ x2 = float(input("Enter the x-coordinate of the second point: "))
 y2 = float(input("Enter the y-coordinate of the second point: "))
 distance = sqrt((x2 - x1)**2 + (y2 - y1)**2)
 print("The distance between the two points is: ", distance)
+
