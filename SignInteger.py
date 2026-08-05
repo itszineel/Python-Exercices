d90f8fbf7122d874fbf089e9e1fc6c918366f72d
unknown <itszineel@gmail.com>
2026-08-05 14:28:12 +0100

    exercice-python

---
+++ SignInteger.py
@@ -0,0 +1,9 @@
+x=int(input("Enter an integer: "))
+y=int(input("Enter another integer: "))
+if x<0 and y<0:
+    print("Both integers are negative.")
+elif x>0 and y>0:
+    print("Both integers are positive.")
+else:
+    print("The integers have different signs.")
+
